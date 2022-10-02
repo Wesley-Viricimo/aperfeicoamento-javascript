d3363325ffc3e6ebab5306edaa80dd83662004f4
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2022-10-02 13:28:50 -0300

    Iniciando com lógica de programação

---
+++ Logica de programação/Operadores de comparação/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== Comparar igualdade (valor)
+=== Igualdade estrita (valor e tipo)
+!= diferente de (valor)
+!== diferente estrito (valor e tipo)
+*/
