5a8b20e3601723147a4ead2974e82ff6e11f0821
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2022-10-29 19:30:42 -0300

    Finalizando IMC

--- Logica de programação/Exercicio IMC HTML CSS/assets/js/main.js
+++ Logica de programação/Exercicio IMC HTML CSS/assets/js/main.js
@@ -25,9 +25,12 @@ form.addEventListener('submit', function (evento) {
 
     const imc = getImc(peso, altura); // => Chamada da função getImc passando como argumento o peso e altura digitados pelo usuário e retornando o valor final para a constante
     const nivelImc = getNivelImc(imc); // => Pega o resultado obtido na constante passando como argumento para retornar ao usuário uma mensagem exibindo o nível IMC que o mesmo se encontra
+    const msgTitulo = getTituloImc(imc);// => Retorna uma mensagem baseada no cálculo IMC obtido
 
     const msg = `Seu IMC é ${imc} (${nivelImc}).`; // => Adicionando na constante msg o resultado do cálculo do IMC e o nível em que o mesmo se encontra 
 
+
+    setTituloResultado(msgTitulo, imc);
     setResultado(msg, true); // => Chamada da função passando como argumento a mensagem  
 });
 
@@ -48,9 +51,45 @@ function getImc(peso, altura) { // => Função que recebe como parâmetro o peso
     return imc.toFixed(2); // => Retorna valor com 2 casas decimais
 }
 
+function getTituloImc(imc) {
+    const nivel = ['Requer atenção', 'Peso ideal',
+    'Requer muita atenção', 'Cuidado, você corre risco de saúde'];
+
+    if (imc >= 39.9) return nivel[3];
+    if (imc >= 34.9) return nivel[2];
+    if (imc >= 29.9) return nivel[2];
+    if (imc >= 24.9) return nivel[0];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
 function criaP() { // => Função criando o elemento de parágrafo
     const p = document.createElement('p');
     return p;
+}
+
+function setTituloResultado(msgTitulo, imc){
+    const resultado = document.querySelector('#titulo-resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    const resultImc = imc; 
+
+    if(resultImc >= 39.9){
+        p.classList.add('bad');
+    } else if (resultImc > 29.9 && resultImc <= 34.9){
+        p.classList.add('muita-atencao');
+    } else if (resultImc > 24.9 && resultImc <= 29.9){
+        p.classList.add('atencao');
+    } else if (resultImc >= 18.5 && resultImc <= 24.9){
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('atencao');
+    }
+
+    p.innerHTML = msgTitulo;
+    resultado.appendChild(p);
 }
 
 function setResultado(msg, isValid) {
@@ -58,7 +97,6 @@ function setResultado(msg, isValid) {
     resultado.innerHTML = ''; // => Limpar a constante após ser exibido o resultado
 
     const p = criaP();
-
 
     if(isValid) { // => Verifica se a variável é verdadeira, se for, adiciona ao parágrafo a formatação da classe CSS
         p.classList.add('paragrafo-resultado');
