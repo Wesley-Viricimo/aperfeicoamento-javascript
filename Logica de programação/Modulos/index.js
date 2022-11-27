079a13aeb27b9ea94c55fc20ab0b5e0086968370
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2022-11-27 09:50:09 -0300

    Iniciando com módulos

---
+++ Logica de programação/Modulos/index.js
@@ -0,0 +1,7 @@
+const calculadora = require("./calculadora"); //recebendo as funções da classe calculadora;
+
+//realizando os cálculos utilizando os funções da classe calculadora;
+console.log(calculadora.soma(10, 20));
+console.log(calculadora.sub(20, 10));
+console.log(calculadora.mult(10, 20));
+console.log(calculadora.div(20, 10));
