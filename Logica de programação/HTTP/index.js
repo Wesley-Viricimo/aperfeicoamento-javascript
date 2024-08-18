7f955e16a606d68ce42a49ba78aa06501d60ac75
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-08-17 21:01:44 -0300

    Seção 3 - Ajustando pacotes e exemplo de atribuição via desestruturação

--- Logica de programação/HTTP/index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-
-//criando servidor e rodando na porta 8181
-http.createServer(function(requisicao, resposta){
-    resposta.end("<h1>Bem vindo ao site!</h1>");
-}).listen(8181);
-console.log("Servidor está rodando");
