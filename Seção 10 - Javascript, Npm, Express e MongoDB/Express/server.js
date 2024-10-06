627f9d935f8c687ebb57ef0e6b2cfd93a392f1e5
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-06 10:33:00 -0300

    Seção 10 - Criando pasta public para arquivos estáticos

--- Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
+++ Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static(path.resolve(__dirname, 'public'))); //SETANDO PASTA PUBLIC RESPONSÁVEL POR ARMAZENAR CONTEÚDOS ESTÁTICOS (CSS, JAVASCRIPT, IMAGENS E ETC)
+
 app.set('views', path.resolve(__dirname, 'src', 'views')); //SETANDO PARA O EXPRESS QUE SERÁ UTILIZADA UMA CAMADA VIEW E INDICANDO O CAMINHO ABSOLUTO DA PASTA (NESTE CASO ESTÁ DENTRO DE SRC)
 app.set('view engine', 'ejs'); //INDICANDO QUE A ENGINE UTILIZADA NA CAMADA DE VIEW SERÁ EJS
 app.use(routes);
