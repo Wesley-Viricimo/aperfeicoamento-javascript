3937e12f241cadd1c3dc20c9b2b58b538f723064
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-06 09:45:04 -0300

    Seção 10 - Routes com express

---
+++ Seção 10 - Javascript, Npm, Express e MongoDB/Express/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./controller/homeController');
+
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+module.exports = route;
