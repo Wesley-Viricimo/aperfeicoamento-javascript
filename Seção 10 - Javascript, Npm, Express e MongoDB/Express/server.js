db23e3bda7ddf627bdeafc0d6b8851c71f36276d
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-06 10:02:48 -0300

    Seção 10 - Criando camada view com ejs

--- Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
+++ Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
 
+app.use(express.urlencoded({extended: true}));
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); //SETANDO PARA O EXPRESS QUE SERÁ UTILIZADA UMA CAMADA VIEW E INDICANDO O CAMINHO ABSOLUTO DA PASTA (NESTE CASO ESTÁ DENTRO DE SRC)
+app.set('view engine', 'ejs'); //INDICANDO QUE A ENGINE UTILIZADA NA CAMADA DE VIEW SERÁ EJS
 app.use(routes);
 
 app.listen(3000, () => {
