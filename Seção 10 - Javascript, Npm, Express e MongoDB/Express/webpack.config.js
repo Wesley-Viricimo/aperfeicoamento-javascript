22228ded3e663d21e2f0d1eaaae410d9459739b4
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-06 11:16:45 -0300

    Seção 10 - Integrando express com webpack

---
+++ Seção 10 - Javascript, Npm, Express e MongoDB/Express/webpack.config.js
@@ -0,0 +1,29 @@
+//ARQUIVO DE CONFIGURAÇÃO DO WEBPACK
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './frontend/main.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{ //ARRAY COM OBJETOS
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }, 
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    devtool: 'source-map'
+}
