176f453710c56c0d559ec13c9b23a23126605cb3
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-13 20:08:46 -0300

    Seção 10 - Adicionando Helmet e csrf

--- Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
+++ Seção 10 - Javascript, Npm, Express e MongoDB/Express/server.js
@@ -12,10 +12,12 @@ mongoose.connect(connectionString) //COMO O MÉTODO CONNECT RETORNA UMA PROMISE 
 const session = require('express-session'); //EXPRESS SESSION SERVE PARA ARMAZENAMENTO DE INFORMAÇÕES QUE SERÁ GUARDADA PELO TEMPO QUE FOR CONFIGURADO ENQUANTO A SESSÃO EXISTIR
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash'); //FLASH É UM RECURSO DE MENSAGEM QUE PODERÁ SER CONSUMIDA APENAS UMA VEZ E APÓS SER CONSUMIDA NÃO SERÁ MAIS EXIBIDA
-
 const routes = require('./routes');
 const path = require('path');
+const helmet = require('helmet');
+const csrf = require('csurf');
 
+app.use(helmet());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.resolve(__dirname, 'public'))); //SETANDO PASTA PUBLIC RESPONSÁVEL POR ARMAZENAR CONTEÚDOS ESTÁTICOS (CSS, JAVASCRIPT, IMAGENS E ETC)
@@ -36,6 +38,7 @@ app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views')); //SETANDO PARA O EXPRESS QUE SERÁ UTILIZADA UMA CAMADA VIEW E INDICANDO O CAMINHO ABSOLUTO DA PASTA (NESTE CASO ESTÁ DENTRO DE SRC)
 app.set('view engine', 'ejs'); //INDICANDO QUE A ENGINE UTILIZADA NA CAMADA DE VIEW SERÁ EJS
+app.use(csrf());
 app.use(routes);
 
 app.on('conexão pronta!', () => { //QUANDO A MENSAGEM DISPARADA NA CONEXÃO FOR RECEBIDA, INDICANDO QUE A CONEXÃO FOI REALIZADA, O SERVIDOR SERÁ INICIADO
