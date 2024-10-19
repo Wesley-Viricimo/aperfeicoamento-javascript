ee0ac755325b0198eeef88269bb2d6c01ee78430
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-19 20:44:01 -0300

    Seção 13 - Adicionando configuração do sequelize e criando migration para tabela de alunos

--- Secao13ApiRest/app.js
+++ Secao13ApiRest/app.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
-import homeRoutes from './src/routes/homeRoute'
+import homeRoutes from './src/routes/homeRoute';
 
 class App {
   constructor() { //NA INSTÂNCIA DA CLASSE, O EXPRESS, MIDLLEWARES E ROUTES SERÃO CHAMADOS
