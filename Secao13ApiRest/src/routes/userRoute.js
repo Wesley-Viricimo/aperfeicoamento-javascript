571e876ec5b719edd1a6c67d532c188335f33e03
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-25 22:50:48 -0300

    Seção 13 - Ajustando aplicação para utilizar banco de dados postgrees

--- Secao13ApiRest/src/routes/userRoute.js
+++ Secao13ApiRest/src/routes/userRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userController from '../controllers/UserController';
 import loginRequired from "../middlewares/loginRequired";
 
+
 const router = new Router();
 
 router.post('/', userController.store);
