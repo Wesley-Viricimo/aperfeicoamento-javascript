bf188fce5ded79dabd55e2336bcf613aedabf03c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-27 11:04:01 -0300

    Seção 13 - Adicionando filtro para upload de arquivos

--- Secao13ApiRest/src/config/multerConfig.js
+++ Secao13ApiRest/src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const geraValorRandomico = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, callback) => { //CONFIGURAÇÃO PARA FILTRAR OS ARQUIVOS
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') { //SE O ARQUIVO NÃO FOR DOS TIPOS JPEG OU JPG
+      return callback(new multer.MulterError('Arquivo precisa ser PGN ou JPG!')); //CHAMADA DO ERRO NO CALLBACK
+    }
+
+    return callback(null, true); //SE O ARQUIVO PASSAR NA VALIDAÇÃO INDICANDO QUE É UMA IMAGEM, SETAR NULO PARA O ERRO E TRUE PARA CONTINUAR A EXECUÇÃO
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads')); //NO CALLBACK DEVE SER INFORMADO O QUE ACONTECERÁ EM CASO DE ERRO (NESTE CASO NADA ACONTECERÁ, POIS ESTÁ NULO) E A PASTA EM QUE OS ARQUIVOS SERÃO SALVOS (__dirname RETORNA A PASTA ATUAL, ..VOLTA UMA PASTA E NESTE CASO ESTÁ VOLTANDO DUAS PASTAS E APONTANDO PARA PASTA UPLOADS DA RAIZ)
--- Secao13ApiRest/src/controllers/FotoController.js
+++ Secao13ApiRest/src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from "multer";
+import multerConfig from "../config/multerConfig";
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => { //MÉTODO UPLOAD É RESPONSÁVEL POR TRATAR O UPLOAD DAS IMAGENS
+      if(err) { //SE DER ERRO NO UPLOAD SERÁ DISPARADA UMA MENSAGEM COM O ERRO (NESTE CASO DARÁ ERRO SE O ARQUIVO NÃO FOR IMAGEM CONFORME CONFIGURADO NO ARQUIVO MULTERCONFIG)
+        return res.status(400).json({
+          errors: [err.code]
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
