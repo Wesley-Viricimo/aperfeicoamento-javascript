38d1fe33a354bfcc27e904c9044f746f4483fe9a
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-10-28 21:38:19 -0300

    Seção 13 - Finalizando configuração para salvar fotos de alunos

--- Secao13ApiRest/src/controllers/FotoController.js
+++ Secao13ApiRest/src/controllers/FotoController.js
@@ -1,18 +1,25 @@
 import multer from "multer";
 import multerConfig from "../config/multerConfig";
 
+import Foto from '../models/Fotos';
+
 const upload = multer(multerConfig).single('foto');
 
 class FotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => { //MÉTODO UPLOAD É RESPONSÁVEL POR TRATAR O UPLOAD DAS IMAGENS
+  store(req, res) {
+    return upload(req, res, async (err) => { //MÉTODO UPLOAD É RESPONSÁVEL POR TRATAR O UPLOAD DAS IMAGENS
       if(err) { //SE DER ERRO NO UPLOAD SERÁ DISPARADA UMA MENSAGEM COM O ERRO (NESTE CASO DARÁ ERRO SE O ARQUIVO NÃO FOR IMAGEM CONFORME CONFIGURADO NO ARQUIVO MULTERCONFIG)
         return res.status(400).json({
           errors: [err.code]
         });
       }
 
-      return res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      return res.json(foto);
     });
   }
 }
